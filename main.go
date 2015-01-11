f0a5f05e13e9e91e2b007f3d0eca8ff9f02a0408
jimzhan <jim.zhan@me.com>
2015-01-11 22:28:00 +0800

    {API} rex.API changes

--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"github.com/goanywhere/rex"
-	"github.com/goanywhere/rex/modules"
-	"github.com/goanywhere/rex/web"	
 )
 
-func index(ctx *web.Context) {
+func index(ctx *rex.Context) {
 	ctx.HTML("index.html")
 }
 
 func main() {
 	app := rex.New()
-	app.Use(modules.LiveReload)
-	app.Use(modules.Static(modules.Options{
-		"URL": "/static",
-	}))
 	app.Get("/", index)
 	app.Run()
 }
