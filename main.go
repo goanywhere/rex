5f54bb8f879029837f0b375dc76365084633c057
jimzhan <jim.zhan@me.com>
2015-01-15 11:43:05 +0800

    default modules for scaffold

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/goanywhere/rex"
+	"github.com/goanywhere/rex/modules"
 	"github.com/goanywhere/rex/web"
 )
 
@@ -11,5 +12,8 @@ func index(ctx *web.Context) {
 
 func main() {
 	rex.Get("/", index)
+	rex.Use(modules.XSRF)
+	rex.Use(modules.Static)
+	rex.Use(modules.LiveReload)
 	rex.Run()
 }
