9254ab65a74156ecc1a564682eb848d727c19287
jimzhan <jim.zhan@me.com>
2015-01-21 21:00:03 +0800

    {api} interfaces updates

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"github.com/goanywhere/rex"
 	"github.com/goanywhere/rex/modules"
-	"github.com/goanywhere/rex/web"
 )
 
-func index(ctx *web.Context) {
+func index(ctx *rex.Context) {
 	ctx.HTML("index.html")
 }
 
 func main() {
-	rex.Get("/", index)
-	rex.Use(modules.XSRF)
 	rex.Use(modules.Static)
 	rex.Use(modules.LiveReload)
+	rex.Get("/", index)
+
 	rex.Run()
 }
