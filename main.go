e3f96592f232bbcf80805730fc5aaabc04d950fb
jimzhan <jim.zhan@me.com>
2015-02-08 16:55:01 +0800

    update to latest api

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/goanywhere/rex"
+import (
+	"github.com/goanywhere/rex"
+	"github.com/goanywhere/rex/web"
+)
 
-func index(ctx *rex.Context) {
-	ctx.HTML("index.html")
+func index(ctx *web.Context) {
+	ctx.Set("echo", "Welcome")
+	ctx.Render("index.html")
 }
 
 func main() {
