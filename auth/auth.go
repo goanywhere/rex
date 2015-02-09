f13504d85d097410e88b8fff2dfbcd82ef0d9894
jimzhan <jim.zhan@me.com>
2015-02-09 17:49:48 +0800

    init auth pkg with password management

---
+++ auth/auth.go
@@ -0,0 +1,61 @@
+/* ----------------------------------------------------------------------
+ *       ______      ___                         __
+ *      / ____/___  /   |  ____  __  ___      __/ /_  ___  ________
+ *     / / __/ __ \/ /| | / __ \/ / / / | /| / / __ \/ _ \/ ___/ _ \
+ *    / /_/ / /_/ / ___ |/ / / / /_/ /| |/ |/ / / / /  __/ /  /  __/
+ *    \____/\____/_/  |_/_/ /_/\__. / |__/|__/_/ /_/\___/_/   \___/
+ *                            /____/
+ *
+ * (C) Copyright 2015 GoAnywhere (http://goanywhere.io).
+ * ----------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ----------------------------------------------------------------------*/
+package auth
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/goanywhere/rex/internal"
+)
+
+var settings = internal.Settings()
+
+func hash(src, key string) []byte {
+	hash := hmac.New(md5.New, []byte(key))
+	hash.Write([]byte(src))
+	return hash.Sum(nil)
+}
+
+// Encrypt creates a new password hash using a strong one-way bcrypt algorithm.
+// Source secret is hahsed with the given key before actual bcrypting.
+func Encrypt(src, key string) (secret string) {
+	cost := settings.Int("auth.encryption.cost", bcrypt.DefaultCost)
+	bytes, err := bcrypt.GenerateFromPassword(hash(src, key), cost)
+	if err == nil {
+		secret = string(bytes)
+	}
+	return
+}
+
+// Verify checks that if the given hash matches the given source secret.
+// Source secret is hahsed with the given key before actual bcrypting.
+func Verify(src, secret, key string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(secret), hash(src, key))
+	if err == nil {
+		return true
+	}
+	return false
+}
---
+++ auth/auth_test.go
@@ -0,0 +1,71 @@
+/* ----------------------------------------------------------------------
+ *       ______      ___                         __
+ *      / ____/___  /   |  ____  __  ___      __/ /_  ___  ________
+ *     / / __/ __ \/ /| | / __ \/ / / / | /| / / __ \/ _ \/ ___/ _ \
+ *    / /_/ / /_/ / ___ |/ / / / /_/ /| |/ |/ / / / /  __/ /  /  __/
+ *    \____/\____/_/  |_/_/ /_/\__. / |__/|__/_/ /_/\___/_/   \___/
+ *                            /____/
+ *
+ * (C) Copyright 2015 GoAnywhere (http://goanywhere.io).
+ * ----------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ----------------------------------------------------------------------*/
+package auth
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestEncrypt(t *testing.T) {
+	src := "I'mAPlainSecret"
+	key := "secretkey@example.com"
+	Convey("auth.Encrypt Test", t, func() {
+		secret := Encrypt(src, key)
+		So(len(secret), ShouldEqual, 60)
+		So(secret, ShouldNotEqual, Encrypt(src, key))
+		So(secret, ShouldNotEqual, Encrypt(src, key))
+		So(secret, ShouldNotEqual, Encrypt(src, key))
+		So(secret, ShouldNotEqual, Encrypt(src, key))
+		So(secret, ShouldNotEqual, Encrypt(src, key))
+	})
+}
+
+func TestVerify(t *testing.T) {
+	src := "I'mAPlainSecret"
+	key := "secretkey@example.com"
+	Convey("auth.Verify Test", t, func() {
+		for index := 0; index < 10; index++ {
+			secret := Encrypt(src, key)
+			So(Verify(src, secret, key), ShouldBeTrue)
+		}
+	})
+}
+
+func BenchmarkEncrypt(b *testing.B) {
+	src := "I'mAPlainSecret"
+	key := "secretkey@example.com"
+	for index := 0; index < b.N; index++ {
+		Encrypt(src, key)
+	}
+}
+
+func BenchmarkVerify(b *testing.B) {
+	src := "I'mAPlainSecret"
+	key := "secretkey@example.com"
+	secret := Encrypt(src, key)
+	for index := 0; index < b.N; index++ {
+		Verify(src, secret, key)
+	}
+}
