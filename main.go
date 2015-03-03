ced5f5eb395e4b9274a3a4bd9dd9d51fb9b13efb
jimzhan <jim.zhan@me.com>
2015-03-03 18:29:59 +0800

    index => apps.Index

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"github.com/goanywhere/rex"
-	"github.com/goanywhere/rex/web"
+
+	"./apps"
 )
 
-func index(ctx *web.Context) {
-	ctx.Set("echo", "Welcome")
-	ctx.Render("index.html")
-}
-
 func main() {
-	rex.Get("/", index)
+	rex.Get("/", apps.Index)
 	rex.FileServer("/static/", "build")
 	rex.Run()
 }
