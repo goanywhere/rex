f3174a21137f7a18cd2e97d9323f1487ba7ddf44
jimzhan <jim.zhan@me.com>
2015-08-19 11:09:09 +0800

    feature(render): Add json rending supports.
    - move Send from server to rex (top level).

--- rex.go
+++ rex.go
@@ -1,6 +1,9 @@
 package rex
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"path"
 
 	"github.com/goanywhere/env"
@@ -9,6 +12,25 @@ import (
 
 // Shortcut for string based map.
 type M map[string]interface{}
+
+// Sends the HTTP response in JSON.
+func Send(w http.ResponseWriter, v interface{}) {
+	var buffer = new(bytes.Buffer)
+	defer func() {
+		buffer.Reset()
+	}()
+
+	w.Header().Set("Content-type", "application/json; charset=utf-8")
+	if err := json.NewEncoder(buffer).Encode(v); err == nil {
+		w.Write(buffer.Bytes())
+	} else {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Renders a view (Pongo) and sends the rendered HTML string to the client.
+// Optional parameters: value, local variables for the view.
+// func Render(filename string, v ...interface{}) {}
 
 func init() {
 	var basedir = fs.Getcd(2)
